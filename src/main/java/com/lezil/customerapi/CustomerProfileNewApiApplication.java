ef41b37d8fcf5a63a187329c942ab9e8fe5cebd9
Lezil George <georgelezil@gmail.com>
2019-04-13 19:30:45 -0500

    Initial commit for Customer profile api

---
+++ src/main/java/com/lezil/customerapi/CustomerProfileNewApiApplication.java
@@ -0,0 +1,13 @@
+package com.lezil.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProfileNewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProfileNewApiApplication.class, args);
+	}
+
+}
