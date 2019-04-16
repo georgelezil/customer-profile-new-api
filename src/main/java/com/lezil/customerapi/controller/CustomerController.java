b58ae431444a00f64114212dc696e4027092cbe0
Lezil George <georgelezil@gmail.com>
2019-04-16 11:33:15 -0500

    Refactored the code for all GET request

--- src/main/java/com/lezil/customerapi/controller/CustomerController.java
+++ src/main/java/com/lezil/customerapi/controller/CustomerController.java
@@ -22,90 +22,60 @@ import com.lezil.customerapi.jpa.PersonJpaRepository;
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
-	
+
 	@Autowired
 	PersonJpaRepository dao;
-	
-	@GetMapping("/hello")
-	public String home() {
-		return "" + new Date() ;
+
+	@RequestMapping(method=RequestMethod.GET, value="/hello")
+	public ResponseEntity<String> home() {
+		return new ResponseEntity<String>("" + new Date(), HttpStatus.OK);
 	}
-	
-	//ResponseEntity  class takes two arguments, one is the returning object itself and other the status code. 
-	//But this adds more code into the controller method and makes it complex with all try catch blocks.
-	/*
+
+	/* Best way */
 	@RequestMapping("/person/{id}")
-	public ResponseEntity<Person> getPersonDetails(@PathVariable int id) {
-		
+	public ResponseEntity<Person> getPersonDetails(@PathVariable int id) throws PersonNotFoundException {
+
 		Person person = null;
-		try {
-			person =  dao.findById(id);
-		}catch(Exception  e){
-			return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<Person>(person,HttpStatus.OK);
-	}
-	*/
-	
-	
-	/* Best way*/
-	@RequestMapping("/person/{id}")
-	public ResponseEntity<Person> getPersonDetails_new(@PathVariable int id) throws PersonNotFoundException {
-		
-		Person person = null;
-		person =  dao.findById(id);
-		if(person == null){
+		person = dao.findById(id);
+		if (person == null) {
 			throw new PersonNotFoundException("No person found");
 		}
-		return new ResponseEntity<Person>(person,HttpStatus.OK);
+		return new ResponseEntity<Person>(person, HttpStatus.OK);
 	}
+
+	@RequestMapping("/person")
+	public ResponseEntity<Person> getPersonDetailsWithRequestParam(@RequestParam(value = "id", defaultValue = "10001") int id) throws PersonNotFoundException {
+
+		Person person = null;
+		person = dao.findById(id);
+		if (person == null) {
+			throw new PersonNotFoundException("No person found");
+		}
+		return new ResponseEntity<Person>(person, HttpStatus.OK);
+	}
+
 	
+	@GetMapping("/persons")
+	public ResponseEntity<List<Person>> getAllPerson() throws PersonNotFoundException {
+
+		List<Person> personlst = dao.findAll();
+		if (personlst != null && personlst.size() == 0) {
+			throw new PersonNotFoundException("No persons found");
+		}
+		return new ResponseEntity<List<Person>>(personlst, HttpStatus.OK);
+
+	}
+
 	@ExceptionHandler(PersonNotFoundException.class)
-    public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex) {
-        ErrorResponse error = new ErrorResponse();
-        error.setErrorCode(HttpStatus.PRECONDITION_FAILED.value());
-        error.setMessage(ex.getMessage());
-        return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
-    }
-	
-	/*Best way End*/
-	
-	/*
-	@RequestMapping("/person1/{id}")
-	public Person getPersonDetails1(@PathVariable int id) {
-		return dao.findById(id);
+	public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex) {
+		ErrorResponse error = new ErrorResponse();
+		error.setErrorCode(HttpStatus.PRECONDITION_FAILED.value());
+		error.setMessage(ex.getMessage());
+		return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
 	}
 
-	
-	@RequestMapping("/person2")
-	public Person getPersonDetails2(@RequestParam(value="id", defaultValue="0") int id) {
-		return dao.findById(id);
-	}
-	
-	@GetMapping("/person3")
-	public Person getPersonDetails3(@RequestParam(value="id", defaultValue="0") int id) {
-		return dao.findById(id);
-	}
-	*/
-	
-	@RequestMapping(method=RequestMethod.GET, value="/persons")
-	public List<Person> getAllPerson() {
-		return dao.findAll();
-	}
-	
-	@GetMapping("/persons1")
-	public List<Person> getAllPerson1() {
-		return dao.findAll();
-	}
-	
-	
-	
-	//public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-	//	return new Greeting(counter.incrementAndGet(),
-	//              String.format(template, name));
-	//}
+	/* Best way End */
 
-	
-	
+
 
 }
